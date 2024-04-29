706cfadc6f85ff1675b25a400071c0b96d7113c3
Aslınur Topcu <aslinurtopcu@Aslnur-MacBook-Air.local>
2024-04-29 19:09:47 +0300

    changes

--- machine/machine/urls.py
+++ machine/machine/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+from dashboard.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard',include('dashboard.urls')),
+    path('',index,name='index'),
+    
 ]
