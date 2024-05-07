65e20da8e6c0ca2c022510caea50abbcfbd114fe
Yusuf <yusufsas3444@gmail.com>
2024-05-08 02:11:40 +0300

    changes

--- machine/dashboard/models.py
+++ machine/dashboard/models.py
@@ -5,24 +5,19 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Interest(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Reader(models.Model):
     id=models.AutoField(primary_key=True)
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
-    
-    
-
-    # interests=models.ManyToManyField(Interest,blank=True)
-   
-
+    interests=models.ManyToManyField(Interest,blank=True)
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
 
 
-
-# class Interest(models.Model):
-
-#     pass  
